34259120e1c3beecb7a37e4c6cb6cc1334ffa1f6
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2019-06-19 15:04:48 -0400

    Updates

---
+++ precache-manifest.6a154d3c72d0e904b42c5613abcd5468.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d38db33912d546b8310e0a90174e388",
+    "url": "/E-Go93/google_booksHW/index.html"
+  },
+  {
+    "revision": "176b8e66cef951e3bfb8",
+    "url": "/E-Go93/google_booksHW/static/css/main.c3966b3f.chunk.css"
+  },
+  {
+    "revision": "d806f9b4d0dd3f278618",
+    "url": "/E-Go93/google_booksHW/static/js/2.38c4fd7c.chunk.js"
+  },
+  {
+    "revision": "176b8e66cef951e3bfb8",
+    "url": "/E-Go93/google_booksHW/static/js/main.8988a8b4.chunk.js"
+  },
+  {
+    "revision": "d183dac5e24cbf8a5c6c",
+    "url": "/E-Go93/google_booksHW/static/js/runtime~main.155a07ed.js"
+  }
+]);
